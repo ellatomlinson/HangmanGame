006c41b7a5df783efb4e8619963455e3521e89a0
ella.tomlinson <ella.tomlinson@ucalgary.ca>
2022-06-14 11:33:50 -0600

    Created drawHangman function

--- src/Hangman.java
+++ src/Hangman.java
@@ -23,6 +23,75 @@ public class Hangman {
                  Press 0 to begin, or X to exit game""");
     }
 
+    /**
+     * Prints hangman according to how many lives remain
+     * @param lives integer representing number of lives player has
+     */
+    private static void drawHangman(int lives){
+        String hangman = "";
+
+        // Create hangman according to number of lives
+        switch (lives){
+            case 1 -> hangman = """
+                     _______
+                    |/      |
+                    |      (_)
+                    |
+                    |
+                    |
+                    |
+                 ___|___""";
+            case 2 -> hangman = """
+                     _______
+                    |/      |
+                    |      (_)
+                    |      \\|
+                    |
+                    |
+                    |
+                 ___|___""";
+            case 3 -> hangman = """
+                     _______
+                    |/      |
+                    |      (_)
+                    |      \\|/
+                    |
+                    |
+                    |
+                 ___|___""";
+            case 4 -> hangman = """
+                     _______
+                    |/      |
+                    |      (_)
+                    |      \\|/
+                    |       |
+                    |
+                    |
+                 ___|___""";
+            case 5 -> hangman = """
+                     _______
+                    |/      |
+                    |      (_)
+                    |      \\|/
+                    |       |
+                    |      /
+                    |
+                 ___|___""";
+            case 6 -> hangman = """
+                     _______
+                    |/      |
+                    |      (_)
+                    |      \\|/
+                    |       |
+                    |      / \\
+                    |
+                 ___|___""";
+        }
+
+        // Print hangman to terminal
+        System.out.println(hangman);
+    }
+
     public static void main(String[] args){
         // Print title screen
         titleScreen();
@@ -33,7 +102,12 @@ public class Hangman {
         while (!Objects.equals(userSelection, "X")){
             // If input is 0, start game
             if (userSelection.equals("0")){
-                System.out.println("Start game");
+                // Set lives to full
+                int lives = 6;
+                // Draw hangman
+                drawHangman(lives);
+                // Select word
+                // Draw board
             }
             // If input is invalid (not 0 or X), print message requesting proper input
             else{
